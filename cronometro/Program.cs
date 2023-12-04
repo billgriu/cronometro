e258e9a6e34bf25ff64761977acd483ac849ec40
Gabriel Schimidt <seu-email@example.com>
2023-12-04 15:14:25 -0300

    inicio cronometro

---
+++ cronometro/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Threading; // Corrigir o nome do namespace
+
+namespace Cronometro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("S = Segundos => 10s = 10 segundos");
+            Console.WriteLine("M = Minutos => 1m = 1 minuto");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("Quanto tempo deseja contar?");
+
+            string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length -1, 1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+            int multiplier = 1;
+
+            if(type == 'm')
+            multiplier = 60;
+
+            if(time == 0)
+                System.Environment.Exit(0);
+
+            PreStart(time * multiplier);
+        }
+
+        static void PreStart(int time){
+            Console.Clear();
+            Console.WriteLine("Preparar...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Apontar...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Já..");
+            Thread.Sleep(2500);
+
+            Start(time);
+        }
+
+        static void Start(int time)
+        {
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                Console.Clear(); // Corrigir o nome do método Clear
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000); // Corrigir o nome da classe Thread
+            }
+
+            Console.Clear();
+            Console.WriteLine("Cronometro finalizado");
+            Thread.Sleep(1000);
+            Menu();
+        }
+    }
+}
